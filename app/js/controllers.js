f550e920c66940a982c22d19dd382fed1b42012d
Arnaud Benard <arnaudbenard13@gmail.com>
2013-12-30 14:08:36 +0100

    Search youtube api and save playlist in local storage

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,34 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+
+  controller('HomeCtrl', ['$scope','Youtube','Playlist',function($scope, Youtube,Playlist) {
+
+    $scope.result = [];
+    $scope.playlist = Playlist.getAll();
+
+    $scope.searchVideo = function(){
+
+        Youtube.search($scope.term, function(response){
+
+            $scope.result.length = 0; // Reset the search array
+
+            var entries = response.feed.entry || [];
+
+            angular.forEach(entries, function(value, key){
+                $scope.result.push(value); // Only get the entries
+            });
+
+        });
+    };
+
+    $scope.addVideo = function(video){
+        $scope.playlist = Playlist.add(video);
+    };
+
+    $scope.resetPlaylist = function(){
+
+    };
 
   }])
   .controller('MyCtrl2', [function() {
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,60 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', ['LocalStorageModule'])
+
+    .value('version', '0.1')
+
+    // Factory that communicates with Youtube API
+    .factory('Youtube',  ['$http', function($http) {
+
+        // Create the query url for the Youtube api
+        var getUrl = function(query) {
+            return 'http://gdata.youtube.com/feeds/api/videos?q=' + query + '&alt=json';
+        };
+
+        return {
+            search: function(term, callback) {
+                return $http.get(getUrl(term)).success(callback);
+            }
+        };
+
+    }])
+
+    // Factory that uses that interacts with Localstorage API (HTML 5)
+    // and has a fallback with cookies for older browsers
+    .factory('localStorage', ['localStorageService', function(localStorageService) {
+
+        return {
+            add: function(key, value) {
+                localStorageService.add(key, value);
+            },
+
+            get: function(key) {
+                return localStorageService.get(key);
+            }
+        };
+
+    }])
+
+    .factory('Playlist',  ['localStorage', function(localStorage) {
+
+        var list = {};
+
+        return {
+            add: function(video){
+                list = localStorage.get('list') || [];
+                list.push(video);
+                console.log(list);
+
+                localStorage.add('list',JSON.stringify(list));
+                return list;
+            },
+            getAll: function(){
+                return localStorage.get('list');
+            }
+
+        };
+
+    }]);
+
